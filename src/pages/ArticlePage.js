2ce907a423e16a5ebaa47e8d70a3b00bc1cffa0b
Marianne Anderson <mganderson13@gmail.com>
2024-05-29 10:37:13 -0500

    useParams to display articles based on name key

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,6 +1,19 @@
+import { useParams } from "react-router-dom";
+import articles from "./article-content";
+
 const ArticlePage = () => {
+  //useParams and destructure to get param string from path in app.js route
+  //no need to have separate page for each article b/c useParams
+    const { articleId }= useParams();
+    const article = articles.find(article => article.name === articleId);
+
     return (
-        <h1>This is the Article page!</h1>
+    <>
+        <h1>{article.title}</h1>
+        {article.content.map(paragraph => (
+            <p>{paragraph}</p>
+        ))}
+    </>
     );
 }
 
