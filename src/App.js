4aceb4364e6c88b77e9190779438332010dd567a
Marianne Anderson <mganderson13@gmail.com>
2024-05-29 10:14:15 -0500

    pages defined and displayed on routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlePage from './pages/ArticlePage';
+import ArticlesListPage from './pages/ArticlesListPage';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <h1>My Blog</h1>
-      <div id="page-body">Welcome to my blog!</div>
+      <div id="page-body">
+       <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/articles" element={<ArticlesListPage />} />
+        <Route path="/article/:id" element={<ArticlePage />} />
+        </Routes>
+      </div>
     </div>
+    </BrowserRouter>
   );
 }
 
