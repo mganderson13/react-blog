3e3c5a1140b44a7c69339a4063b371f5e5c9ee16
Marianne Anderson <mganderson13@gmail.com>
2024-06-04 10:58:25 -0500

    use axios to request articleInfo from db

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,11 +1,28 @@
+import { useState, useEffect } from 'react';
 import { useParams } from "react-router-dom";
+import axios from 'axios';
 import articles from "./article-content";
 import NotFoundPage from "./NotFoundPage";
 
 const ArticlePage = () => {
-  //useParams and destructure to get param string from path in app.js route
-  //no need to have separate page for each article b/c useParams
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] })
+    //useParams and destructure to get param string from path in app.js route
+    //no need to have separate page for each article b/c useParams
     const { articleId }= useParams();
+
+    useEffect(() => {
+        //need internal function b/c fo async/await
+        const loadArticleInfo = async () => {
+            const response = await axios.get(`/api/articles/${articleId}`);
+            const newArticleInfo = response.data;
+            setArticleInfo(newArticleInfo);
+        }
+
+        //call async function to make it run
+        loadArticleInfo();
+    });
+    
+
     const article = articles.find(article => article.name === articleId);
 
     if( !article) {
@@ -14,6 +31,7 @@ const ArticlePage = () => {
     return (
     <>
         <h1>{article.title}</h1>
+        <p>This article has {articleInfo.upvotes} upvotes</p>
         {article.content.map((paragraph, i) => (
             <p key={i}>{paragraph}</p>
         ))}
