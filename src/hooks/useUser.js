0b036c8cfe43422d87b0dbaaa6b121db2cebb507
Marianne Anderson <mganderson13@gmail.com>
2024-06-17 15:55:17 -0500

    created useUser custom hook

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -1,1 +1,22 @@
 //custom hook to see which user is logged in 
+import { useState, useEffect } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+const useUser = () => {
+
+    const [user, setUser] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(getAuth(), user => {
+            setUser(user);
+            setIsLoading(false);
+        })
+
+        return unsubscribe;
+    }, []);
+
+    return { user, isLoading };
+}
+
+export default useUser;
