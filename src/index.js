d6d1d50e0e2baf78603f4fe23cb977bf415cf842
Marianne Anderson <mganderson13@gmail.com>
2024-06-17 14:42:46 -0500

    added firebase auth

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC9BS1DQ70sCpTk1LN5VfyvHwZLQI44Kq0",
+  authDomain: "react-blog-dcfc9.firebaseapp.com",
+  projectId: "react-blog-dcfc9",
+  storageBucket: "react-blog-dcfc9.appspot.com",
+  messagingSenderId: "807247465439",
+  appId: "1:807247465439:web:19f2a98c46baa5a221526a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
